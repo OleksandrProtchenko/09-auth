387759f16573f4ff34f20529f5eaac88cf0ece66
OleksandrProtchenko <alexdevelop92@gmail.com>
2026-03-31 04:48:13 +0300

    add path to api

--- app/(private routes)/notes/[id]/NoteDetails.client.tsx
+++ app/(private routes)/notes/[id]/NoteDetails.client.tsx
@@ -5,7 +5,7 @@ import { useParams } from "next/navigation";
 import css from "./NoteDetails.module.css";
 import NotePreview from "@/components/NotePreview/NotePreview";
 import { useRouter } from "next/navigation";
-import { fetchNoteById } from "@/lib/clientApi";
+import { fetchNoteById } from "@/lib/api/clientApi";
 
 export default function NoteDetailsClient() {
   const { id } = useParams<{ id: string }>();
