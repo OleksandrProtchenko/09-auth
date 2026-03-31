430ea43583014ba7bbcaf97bd7005d7d82b89d41
OleksandrProtchenko <alexdevelop92@gmail.com>
2026-03-31 04:46:38 +0300

    add path

--- app/(private routes)/notes/[id]/page.tsx
+++ app/(private routes)/notes/[id]/page.tsx
@@ -5,7 +5,7 @@ import {
 } from "@tanstack/react-query";
 import NoteDetailsClient from "./NoteDetails.client";
 import { Metadata } from "next";
-import { fetchNoteById } from "@/lib/clientApi";
+import { fetchNoteById } from "@/lib/api/clientApi";
 
 interface NoteDetailsProps {
   params: Promise<{ id: string }>;
